c7e792bf7833f11e6ec3d302bfd935078fc94e1b
hung <nguyenbahung012@gmail.com>
2020-07-18 18:10:35 +0700

    Commit add classify

---
+++ app/src/main/java/com/hthh/bookapp/fragment/ClassifyFragment.java
@@ -0,0 +1,23 @@
+package com.hthh.bookapp.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ClassifyFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
