9e1255550ce5a1fb6f8a1544b61608ab55afb401
Nguyen Hung <nguyenbahung012@gmail.com>
2020-08-05 23:29:30 +0700

    commit update app

--- app/src/main/java/com/hthh/bookapp/Key.java
+++ app/src/main/java/com/hthh/bookapp/Key.java
@@ -1,7 +1,7 @@
 package com.hthh.bookapp;
 
 public interface Key {
-    String BASE_URL = "http://192.168.43.233/";
-    String DOMAIN = "http://192.168.43.233/story_manager/"; //IPV4 Wifi riêng Nhung
+    String BASE_URL = "http://192.168.1.21/";
+    String DOMAIN = "http://192.168.1.21/story_manager/"; //IPV4 Wifi riêng Nhung
     String SQL = "runsql.php";
 }
